c697bf94d5cf45d0a4ad5bd93ede4ecca644a20e
Bart van Beckhoven <b.vanbeckhoven1@student.avans.nl>
2020-05-26 14:25:57 +0200

    Cardfinder layout done

--- App/app/src/main/java/com/example/android/cardgame/kaartvinder.java
+++ App/app/src/main/java/com/example/android/cardgame/kaartvinder.java
@@ -9,6 +9,6 @@ public class kaartvinder extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kaartvinder);
+        setContentView(R.layout.activity_cardfinder);
     }
 }
