7596e344ae96d94931698b362f6cc4dc4d9b96ef
Bart van Beckhoven <b.vanbeckhoven1@student.avans.nl>
2020-05-26 11:08:26 +0200

    Added activity_kaartvinder
    Kaartvinder activity aangemaakt met bijbehorende .java

---
+++ App/app/src/main/java/com/example/android/cardgame/kaartvinder.java
@@ -0,0 +1,14 @@
+package com.example.android.cardgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kaartvinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kaartvinder);
+    }
+}
