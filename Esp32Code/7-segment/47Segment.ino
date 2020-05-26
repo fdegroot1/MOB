f98b1b6d3322a7cd56be1fdddd009464192db5d3
Redouan Khanfous <r.khanfous@student.avans.nl>
2020-05-26 13:06:33 +0200

    LCD-code

--- Esp32Code/7-segment/47Segment.ino
+++ Esp32Code/7-segment/47Segment.ino
@@ -66,3 +66,36 @@ void loop() {
   digitalWrite(f, LOW);
   digitalWrite(g, HIGH);
 }
+
+
+Code voor de lcd scherm
+
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // Robojax LCD1602 Test
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  // Robojax LCD1602 Test
+}
+
+void loop() {
+  // Robojax LCD1602 Test
+    lcd.setCursor(0, 0);
+    lcd.print("Robojax LCD Test");
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print("Hello World!");
+
+
+  delay(500);
+  // Robojax LCD1602 Test
+}
