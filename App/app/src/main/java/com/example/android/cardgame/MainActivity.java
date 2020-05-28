e38a554cf7f93e6359360139c9fc87c90e61480a
Wout02 <w.geuze@student.avans.nl>
2020-05-28 10:46:58 +0200

    Added basic option menu

--- App/app/src/main/java/com/example/android/cardgame/MainActivity.java
+++ App/app/src/main/java/com/example/android/cardgame/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.android.cardgame;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -43,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.options_item:
                 setContentView(R.layout.activity_options);
                 return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
