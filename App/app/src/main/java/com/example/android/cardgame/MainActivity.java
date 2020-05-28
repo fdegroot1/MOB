8f391b0c53e0668fb3ec0f00089fd69c240bc6aa
Wout02 <w.geuze@student.avans.nl>
2020-05-28 11:29:14 +0200

    Added backbutton to toolbar.

--- App/app/src/main/java/com/example/android/cardgame/MainActivity.java
+++ App/app/src/main/java/com/example/android/cardgame/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.cardgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         NavController NavController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, NavController);
+
     }
 
     @Override
@@ -34,10 +37,12 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.gamerules_item:
-               setContentView(R.layout.activity_gamerules);
+                Intent intent = new Intent(this, GamerulesActivity.class);
+                startActivity(intent);
                 return true;
             case R.id.options_item:
-                setContentView(R.layout.activity_options);
+                Intent intent2 = new Intent(this, OptionsActivity.class);
+                startActivity(intent2);
                 return true;
 
             default:
