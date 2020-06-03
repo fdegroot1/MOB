0008709277489496faad6a23d3d71201151949ea
Bart van Beckhoven <b.vanbeckhoven1@student.avans.nl>
2020-06-03 10:21:09 +0200

    Added some onClick methods

--- App/app/src/main/java/com/example/android/cardgame/battle.java
+++ App/app/src/main/java/com/example/android/cardgame/battle.java
@@ -3,6 +3,7 @@ package com.example.android.cardgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class battle extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class battle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
+    }
+
+    public void connectButton(View view) {
+    }
+
+    public void chooseTeam(View view) {
+    }
+
+    public void beginGame(View view) {
     }
 }
